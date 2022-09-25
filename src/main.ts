acdd7e0d4a326d3b1faf81bfec0c3cf580bd33d7
jonylopezm <jonslopez06@gmail.com>
2022-09-24 20:50:05 -0600

    se agregaron ejemplos de dipo de datos y otros

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,14 @@
-console.log('Hola buenas noches');
+import Tipos from "./tipos";
+import { contarPosiciones, imprimirNombre } from './arreglos';
+import { listarArreglo, listarString, listarInLoop} from './arreglos2';
+
+//imprimirNombre();
+//contarPosiciones();
+//listarArreglo();
+//listarString();
+listarInLoop();
+
+//const tipos = new Tipos(2,2);
+
+//console.log(`La suma de 4 y 4 es: ${tipos.sumar(4,4)}`);
+
